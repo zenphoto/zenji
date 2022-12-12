94d36f1b421cb4ed7a2a4a90fc2a4edc9d963b26
acrylian <wartgart@gmx.net>
2022-12-12 18:06:54 +0100

    Change version date

--- zenji/theme_description.php
+++ zenji/theme_description.php
@@ -2,6 +2,6 @@
 $theme_description['name'] = ucfirst(basename(dirname(__FILE__)));
 $theme_description['author'] = 'gjr';
 $theme_description['version'] = '1.1';
-$theme_description['date'] = '7/27/2014';
+$theme_description['date'] = '2022-12-12';
 $theme_description['desc'] = 'A customizable, responsive (mobile-first), Zenphoto/Zenpage theme.';
 ?>
